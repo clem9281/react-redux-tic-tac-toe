6894a232e99ca6bf526c600853ca3e57a678c221
clem9281 <clem9281@gmail.com>
2019-06-17 16:15:04 -0700

    basic game structure

---
+++ game/src/components/Board.js
@@ -0,0 +1,72 @@
+import React from "react";
+import Square from "./Square";
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+  handleClick = i => {
+    const squares = this.state.squares.slice();
+    console.log(Boolean(squares[i]));
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  };
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <section className="board">
+        <div className="status">{status}</div>
+        <div className="game">
+          {this.state.squares.map((square, i) => (
+            <Square
+              value={square}
+              handleClick={this.handleClick}
+              key={i}
+              id={i}
+            />
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Board;
